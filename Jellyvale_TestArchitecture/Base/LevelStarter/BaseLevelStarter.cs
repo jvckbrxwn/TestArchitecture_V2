9efca1c1a380fff74e6197731ff1f6b1ffa85d2f
Kokoshko Stanislav <ctacoh15l@gmail.com>
2024-07-02 19:11:15 +0300

    Move initializer to base folder

--- Jellyvale_TestArchitecture/Base/LevelStarter/BaseLevelStarter.cs
+++ Jellyvale_TestArchitecture/Base/LevelStarter/BaseLevelStarter.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections.Generic;
-
 namespace Jellyvale_TestArchitecture
 {
 	public enum LoadedLevelType
@@ -16,17 +13,5 @@ namespace Jellyvale_TestArchitecture
 		public abstract void Execute();
 
 		public abstract void ResetCurrentLoop();
-	}
-
-	public abstract class BaseLevelInitializer : ILevelInitializer
-	{
-		protected abstract IDictionary<LoadedLevelType, BaseLevelGenerator> LevelGenerators { get; }
-
-		public virtual void Execute(Match3LevelParams @params)
-		{
-			Console.WriteLine($"{GetType()} executing");
-			Console.WriteLine(@params.LevelId);
-			LevelGenerators[@params.Type].GetLevelData();
-		}
 	}
 }
